84ef52a3037494723a564547d047ae8f21893007
Jwanjema <wawerujoe@gmail.com>
2025-08-06 14:46:06 +0300

    Create grasshopper.py

---
+++ grasshopper.py
@@ -0,0 +1,12 @@
+def greet(name, owner):
+    if name == owner :
+        return "Hello boss"
+    else :
+        return "Hello guest"
+    
+    
+    
+# if name is equal to owner 
+# return "Hello Boss"
+#else
+#return "Hello guest"
